4b53dea58c2e2251b8c5a4c0119459c805899b5c
Matthias Jarsch <matthias.jarsch@student.kit.edu>
2025-12-11 16:19:59 +0000

    Clean up unnecessary and depracted parts of the setup.py

--- setup.py
+++ setup.py
@@ -28,30 +28,6 @@ extensions = [
 ]
 
 setup(
-    name="pyrvo2",
-    version="1.0.0",
-    description="Python bindings for RVO2 library",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/yourusername/yourpackage",
-    author="Your Name",
-    author_email="your.email@example.com",
     ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
     cmdclass={'build_ext': BuildRvo2Ext},
-    python_requires=">=3.8",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Cython',
-        'Topic :: Games/Entertainment :: Simulation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    install_requires=[
-        'Cython',  # Include other dependencies if necessary
-    ],
 )
